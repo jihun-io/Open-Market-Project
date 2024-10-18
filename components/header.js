978d39af115bbf610637a1ca4209f7be9eb8ef4f
김지훈 <kim@jihun.io>
2024-10-18 09:11:14 +0900

    헤더 오류 수정
    로그인 페이지 혹은 회원가입 페이지에서 오류가 발생하는 문제 수정

--- components/header.js
+++ components/header.js
@@ -96,11 +96,10 @@ export default function Header() {
 }
 
 export function headerBtns() {
-  // 메인 페이지나 상세 페이지에서 마이페이지 버튼이 존재하는 경우
+  // route-login 혹은 route-join 페이지가 아닐 경우
   if (
-    (document.body.classList.toString().includes("route-items") ||
-      document.body.classList.toString().includes("route-home")) &&
-    !!localStorage.getItem("encryptedRefresh")
+    !window.location.pathname.includes("login") &&
+    !window.location.pathname.includes("join")
   ) {
     const myPageBtn = document.querySelector("button.my-page-btn");
     const myPageImg = document.querySelector("button.my-page-btn img");
@@ -124,5 +123,31 @@ export function headerBtns() {
         myPageImg.src = "/images/icon-user.svg";
       }
     });
+
+    // 메인 페이지나 상세 페이지에서 마이페이지 버튼이 존재하는 경우
+    if (!!localStorage.getItem("encryptedRefresh")) {
+      const myPageBtn = document.querySelector("button.my-page-btn");
+      const myPageImg = document.querySelector("button.my-page-btn img");
+
+      myPageBtn.addEventListener("click", () => {
+        const dropdown = document.querySelector("ul.dropdown");
+        dropdown.classList.toggle("active");
+        myPageBtn.classList.toggle("active");
+        if (myPageBtn.classList.contains("active")) {
+          myPageImg.src = "/images/icon-user-2.svg";
+        } else {
+          myPageImg.src = "/images/icon-user.svg";
+        }
+      });
+
+      document.addEventListener("click", (e) => {
+        if (!e.target.closest("button.my-page-btn")) {
+          const dropdown = document.querySelector("ul.dropdown");
+          dropdown.classList.remove("active");
+          myPageBtn.classList.remove("active");
+          myPageImg.src = "/images/icon-user.svg";
+        }
+      });
+    }
   }
 }
