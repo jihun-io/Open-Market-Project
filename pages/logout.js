d581a7cad49f7dd3e00c41278fba3d8819ad1203
김지훈 <kim@jihun.io>
2024-10-18 13:32:33 +0900

    로그아웃 시 세션스토리지 내 토큰 삭제

--- pages/logout.js
+++ pages/logout.js
@@ -7,6 +7,8 @@ export default function Logout() {
   localStorage.removeItem("user_type");
   localStorage.removeItem("encryptedRefresh");
 
+  sessionStorage.removeItem("encryptedAccess");
+
   location.href = "/";
   return {
     title: "로그아웃 - HODU",
