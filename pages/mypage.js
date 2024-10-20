3a1bba3080a698b9df1b1e2f6e02887112353e96
김지훈 <kim@jihun.io>
2024-10-20 19:23:31 +0900

    판매자의 주문 목록 링크 비활성화

--- pages/mypage.js
+++ pages/mypage.js
@@ -1,6 +1,16 @@
 import Header from "../components/header.js";
 import Footer from "../components/footer.js";
 
+function orderLink() {
+  if (localStorage.getItem("user_type") === "BUYER") {
+    return /*html*/ `
+      <li><a href="/mypage/order">주문 목록</a></li>
+    `;
+  } else {
+    return /*html*/ ``;
+  }
+}
+
 export default function Mypage() {
   if (!sessionStorage.getItem("encryptedAccess")) {
     location.href = "/login";
@@ -14,7 +24,7 @@ export default function Mypage() {
       <article>
         <h2>마이페이지</h2>
         <ul>
-          <li><a href="/mypage/order">주문 목록</a></li>
+          ${orderLink()}
           <li><a href="/mypage/account">회원 정보 수정</a></li>
         </ul>
       </article>
